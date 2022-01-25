486f6a7d9e25b54540e57fc3636b416f6396764d
Giuseppe Attanasio <giuseppe.attanasio@polito.it>
2022-01-25 15:07:17 +0100

    This commit adds a bunch of features: - reading the room map from a local file - optional styling - room placement is done on a different dataframe wrt the one that is being enumerated. - Cattedra --> Professor Desk - repeat the same class and assign a numerical integer in front (e.g., specify multiple times "P" and get "1P", "2P", etc.) - removes the need of placeholder "x". Why they where needed? - adds references for P classrooms in the yaml file

--- find_disposition.py
+++ find_disposition.py
@@ -12,6 +12,7 @@ def get_args():
     parser.add_argument('rooms', nargs='*', type=str, default=["Rb", "R"], help="La lista delle aule nelle quali dividere gli studenti.")
     parser.add_argument('-s', '--students', type=str, default="Lista_Prenotati_294874.xlsx", help="Percorso del file Excel contenente l'elenco degli studenti prenotati.")
     parser.add_argument('-n', '--name', type=str, default="COD_yyyymmdd", help="Nome dei files di output. Default: '<COD>_yyyymmdd'.")
+    parser.add_argument("--style", action="store_true", default="Add style to the resulting sheet.")
     args = parser.parse_args()
     return args
 
@@ -20,39 +21,75 @@ def snake_j(row, i, j):
     return row.index[len(row) - row.index.get_loc(j) - 1] if i % 4 == 2 else j
 
 
-def stamp_id(a_name, sheet_name, window, matricole, prenotati):
-    row_lims = re.findall(r"\d+", window)
+def stamp_id(room_name, config, matricole, prenotati):
+    window = config["position"]
+    
+    rows = list(map(int, re.findall(r"\d+", window)))
     cols = re.findall(r"[a-zA-Z]", window)
     cols = [ord(a.lower())-97 for a in cols]
 
-    sheet_id = "1yuDN40n8pcoP2FgjUwuR6oxKvNRSa2jbJTVhOrsB5_A"
-    sheet_name = sheet_name.replace(" ", "%20")
-    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
-
-    aula = pd.read_csv(url, usecols=range(cols[0], cols[1]+1),
-                               index_col=0,  skiprows=int(row_lims[0])-1,
-                              ).iloc[:(int(row_lims[1])-int(row_lims[0])), :]
-    if len(matricole) < np.nansum(aula.values):
-        matricole += ["x" for _ in range(int(np.nansum(aula.values)) - len(matricole))]
+    if "filename" in config:
+        aula = pd.read_csv(config["filename"], sep="\t", header=None)
         
+    else: # retrieve from Google's API
+        # this might return a wrong CSV, beware
+        sheet_id = "1yuDN40n8pcoP2FgjUwuR6oxKvNRSa2jbJTVhOrsB5_A"
+        sheet_name = sheet_name.replace(" ", "%20")
+        url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+        aula = pd.read_csv(
+            url,
+            header=None
+        )
+
+    aula = aula.iloc[rows[0]: rows[1], cols[0]: cols[1] + 1].copy()
+    aula = aula.set_index(aula.columns[0])
+
+    slots = (~aula.isna()).sum().sum()
+    # if len(matricole) < nans:
+    #     matricole += ["x" for _ in range(nans - len(matricole))]
+        
+    print(slots, len(matricole))
+    
     aula = aula.rename(columns={c: "" for c in aula.columns if type(c)==str and "Unnamed" in c})
     aula = aula.rename(index = {i: chr(int(i)+64) if i>0 else np.NaN for i in aula.index})
-
-    for i, row in aula.sort_index().iterrows():
-        for j, place in row.iteritems():
-            if not np.isnan(place):
-                curr_matricola = matricole.pop(0)
-                aula.loc[i, snake_j(row, ord(i), j)] = curr_matricola
-                if not curr_matricola == "x":
-                    prenotati.loc[curr_matricola, ["AULA", "POSTO"]] = a_name, f"{i}{snake_j(row, ord(i), j)}"
-    aula.loc[np.NaN] = ""
-    aula[""] = ""
     
-    aula = aula.append(pd.Series(
-        {c:"Cattedra" if c==aula.columns[aula.shape[1]//2] else "" for c in aula.columns}, 
+    #import IPython; IPython.embed(); exit(-1)
+    
+    placement = aula.copy()
+    
+    for i, row in aula.sort_index().iterrows():
+    
+        row = row.astype(float)
+        if not matricole:
+            break
+        
+        for j, place in row.iteritems():
+            
+            if not matricole:
+                break
+                
+            # import IPython; IPython.embed(); exit(-1)
+            if place == 1: # force notation here
+                curr_matricola = matricole.pop(0) 
+                placement.loc[i, j] = str(curr_matricola)
+                
+                if not curr_matricola == "x":
+                    prenotati.loc[curr_matricola, ["AULA", "POSTO"]] = room_name, f"{i}{snake_j(row, ord(i), j)}"
+
+        #print(placement)
+        #exit(-1)
+        
+    placement = placement.fillna("")
+    placement[""] = ""
+    
+    placement = placement.append(pd.Series(
+        {c:"Professor Desk" if c==placement.columns[placement.shape[1]//2] else "" for c in placement.columns}, 
         name=""
     ))
-    return aula, matricole
+    
+    # print(placement)
+    return placement, matricole
 
 
 def styled_seats(x):
@@ -77,17 +114,33 @@ def main(args):
     prenotati = prenotati.assign(AULA=np.NaN, POSTO=np.NaN)
 
     matricole = prenotati[~prenotati.NOTE.str.contains("Esame online", na=False)].index.to_list()
-
+    
     with open("riferimenti_aule.yaml", "r") as f:
         riferimenti = yaml.load(f, yaml.SafeLoader)
 
     writer = pd.ExcelWriter(f"{args.name}_disposizioni.xlsx")
-    for i, a in enumerate(args.rooms):
-        sheet_name, window = riferimenti.get(a).values()
-        aula, matricole = stamp_id(a, sheet_name, window, matricole, prenotati)
+    
+    if len(args.rooms) > 1 and len(set(args.rooms)) == 1:
+        room_names = [f"{i+1}{r}" for i, r in enumerate(args.rooms)]
+    else:
+        room_names = args.rooms
         
-        aula.style.apply(styled_seats, axis=None).to_excel(writer, sheet_name=f"Aula {a}")
+    print("Room names:", room_names)
+    
+    for i, (a, room_name) in enumerate(zip(args.rooms, room_names)):
+        config = riferimenti.get(a)
+        if "position" not in config:
+            raise RuntimeError("Specify a position window in YAML reference file!")
+        
+        aula, matricole = stamp_id(room_name, config, matricole, prenotati)
+        
+        if args.style:
+            aula = aula.style.apply(styled_seats, axis=None)
+        
+        aula.to_excel(writer, sheet_name=f"Aula_{room_name}")
 
+    # remove placeholders "x"
+    # matricole = [m for m in matricole if m != "x"]
     assert len(matricole) == 0, f"Aule indicate non sufficienti, rimangono {len(matricole)} studenti ({matricole})"
     prenotati.to_excel(f"{args.name}_prenotati.xlsx")
     writer.save()
